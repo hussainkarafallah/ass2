60bdfdfa505835339cc8aaccd92f4c3dd71b55cd
Hussain Kara Fallah <hussain.karafallah@transferwise.com>
2022-09-30 22:44:11 +0300

    asd

--- transposematvec.cu
+++ transposematvec.cu
@@ -11,7 +11,7 @@ const unsigned int threads_per_block = 1024;
 cublasHandle_t handle;
 cublasStatus_t stat = cublasCreate(&handle);
 
-
+/*
 __global__ void dot_product(
   const int M,
   const int N,
@@ -29,17 +29,17 @@ __global__ void dot_product(
   }
 
   Y[row] = result;
-}
+}*/
 
 void initVec(const int N , float *vec , const float val){
   for(unsigned int i = 0 ; i < N ; i++)
     vec[i] = val;
 }
 
-void initMat(const int M , const int N , float *mat , float val){
+void initMat(const int M , const int N , float *mat){
   for(unsigned int row = 0 ; row < M ; row++){
     for(unsigned int col = 0 ; col < N ; col++){
-      mat[col * M + row] = val;
+      mat[col * M + row] = (row / 100);
     }
   }
 }
@@ -51,28 +51,28 @@ void benchmark_triad(const std::size_t M , const std::size_t N , const unsigned 
   const float val = 97;
 
   float *h_A = (float*) malloc(M * N * sizeof(float));
-  float *h_X = (float*) malloc(N * sizeof(float));
-  float *h_Y = (float*) malloc(M * sizeof(float));
+  float *h_X = (float*) malloc(M * sizeof(float));
+  float *h_Y = (float*) malloc(N * sizeof(float));
   
-  initVec(N , h_X , 1);
-  initVec(M , h_Y , 0);
-  initMat(M , N , h_A , val);
-
+  // initialize vector to all ones
+  initVec(M , h_X , 1);
+  // initialize matrix so ith row has integer part of (i/100)
+  // first 100 columns will be zeroes, second 100 columns will be ones ... etc
+  initMat(M , N , h_A);
+  // expected result of ith scalar of resulting vector is M * int(i/100)
+  initVec(N , h_Y , 0);
 
   float *d_A , *d_X , *d_Y;
-  // allocate matrix and vector
-  
-  cudaMalloc(&d_X, N * sizeof(float));
-  cudaMalloc(&d_Y, M * sizeof(float));
+
+  cudaMalloc(&d_X, M * sizeof(float));
+  cudaMalloc(&d_Y, N * sizeof(float));
   cudaMalloc(&d_A, M * N * sizeof(float));
     
-  cudaMemcpy(d_X , h_X , N * sizeof(float) ,cudaMemcpyHostToDevice);
+  cudaMemcpy(d_X , h_X , M * sizeof(float) ,cudaMemcpyHostToDevice);
   cudaMemcpy(d_A , h_A , M * N * sizeof(float) ,cudaMemcpyHostToDevice);
 
   
-  const unsigned int n_blocks = (M + threads_per_block - 1) / threads_per_block;
-
-  std::vector<float> result_host(M);
+  std::vector<float> result_host(N);
 
   const unsigned int n_tests = 30;
   double best = 1e10, worst = 0, avg = 0;
@@ -94,6 +94,7 @@ void benchmark_triad(const std::size_t M , const std::size_t N , const unsigned 
             }
         }
         else{
+          const unsigned int n_blocks = (M + threads_per_block - 1) / threads_per_block;
           dot_product<<<n_blocks, threads_per_block>>>(M , N , d_A , d_X ,d_Y);
         }
       }
@@ -112,13 +113,28 @@ void benchmark_triad(const std::size_t M , const std::size_t N , const unsigned 
     }
 
   // Copy the result back to the host
-  cudaMemcpy(result_host.data(), d_Y, M * sizeof(float), cudaMemcpyDeviceToHost);
+  cudaMemcpy(result_host.data(), d_Y, N * sizeof(float), cudaMemcpyDeviceToHost);
   
-  float targetResult = N * val;
-  if (result_host[0] != targetResult)
-    std::cout << "Error in computation, got "
-              << result_host[0] << " instead of "<< targetResult
-              << std::endl;
+  int bad_result = 0;
+  for(int i = 0 ; i < N ; i++){
+    int expected = (i / 100) * M;
+    if(result_host[i] != expected)
+      bad_result = 1;
+  }
+
+  for(int i = 0 ; i < M ; i++){
+    for(int j = 0 ; j < N ; j++){
+      printf("%lf " , h_A[j * M + i]);
+    }
+    printf("\n");
+  }
+  for(int j = 0 ; j < N ; j++){
+    printf("%lf " , h_X[j]);
+  }
+  printf("\n");
+  
+  if (bad_result)
+    std::cout << "Error in computation, some scalar in the vector is not as expected" << std::endl;
 
   // Free the memory on the device
   cudaFree(d_A);
@@ -145,8 +161,8 @@ int main(int argc, char **argv)
   //benchmark_triad(5000 , 5000 , 30, 0);
   
   printf("CUBLAS :: \n");
-  for(int n = 100 ; n <= 10000 ; n = (1 + n * 1.1)){
-     benchmark_triad(n , n , 30 , 1);
+  for(int n = 32 ; n <= 32 ; n = (1 + n * 1.1)){
+     benchmark_triad(n , n , 1 , 1);
   }
   
   return 0;
