791ec298afe10c9af71db5cc1ae7faa6af3c1b8d
Hussain Kara Fallah <hussain.karafallah@transferwise.com>
2022-09-30 21:58:24 +0300

    asd

--- matmul.cu
+++ matmul.cu
@@ -6,7 +6,7 @@
 #include <vector>
 #include "cublas_v2.h"
 
-const unsigned int BLOCK_DIM = 8;
+const unsigned int BLOCK_DIM = 32;
 
 cublasHandle_t handle;
 cublasStatus_t stat = cublasCreate(&handle);
